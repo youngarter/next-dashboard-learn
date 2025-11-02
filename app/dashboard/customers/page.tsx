309a6f2b34afce98bbfb8d9159e0777a461515ed
youngarter <youngesida@gmail.com>
2025-11-02 01:54:40 +0100

    adding the missing files

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function customersPage() {
+  return <h1 className="text-2xl font-bold">Customers Page</h1>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function invoicesPage() {
+  return <h1 className="text-2xl font-bold">Invoices Page</h1>;
+}
