4701b5dfa7e00ebc31cfe8dfae28813889b1d25f
youngarter <youngesida@gmail.com>
2025-11-03 00:50:47 +0100

    the chapter 12 of the next course

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 6000));
+    //console.log('Fetching revenue data...');
+   // await new Promise((resolve) => setTimeout(resolve, 6000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -34,8 +34,8 @@ export async function fetchLatestInvoices() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 4000));
+    // console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 4000));
 
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
